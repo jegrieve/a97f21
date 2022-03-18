a05fee9ec60d7133c61a754639c6db44a14c4eda
James Grieve <jegrieve7@gmail.com>
2022-03-18 12:20:42 -0700

    update associations to create a many-to-many  between User and Conversation through ConversationMember

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,15 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const ConversationMember = require("./conversationMember");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
+User.belongsToMany(Conversation, { through: ConversationMember });
+Conversation.belongsToMany(User, { through: ConversationMember });
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
